fa299a0b2926e0bb0ad837db328180f37441f2f8
le_hoang_viet <le_hoang_viet@monorevo.jp>
2025-05-02 14:02:51 +0700

    customised_envs: set 1st state = (0, 0) in customised_env.py

--- Customised_Envs/customised_env.py
+++ Customised_Envs/customised_env.py
@@ -10,13 +10,13 @@ class HelicopterControlEnv(gym.Env):
     def __init__(self):
         super().__init__()
         self.grid_size = 50
-        self.max_step_size = 100
+        self.max_step_size = 150
         self.action_space = spaces.Discrete(5)  # up, left, right, up-left, up-right
         self.observation_space = spaces.Box(
             low=0, high=self.grid_size - 1, shape=(4,), dtype=np.int32
         )
-        self.gravity = 0.5
-        self.step_size = 2
+        self.gravity = 3
+        self.step_size = 5
 
         self.agent_pos = np.array([0, 0])  # (x, y)
         self.goal_pos = np.array([0, 0])
@@ -24,31 +24,23 @@ class HelicopterControlEnv(gym.Env):
 
     def reset(self, goal=None, seed=None, options=None):
         super().reset(seed=seed)
-        self.agent_pos = np.array(
+        self.agent_pos = np.array([0, 0])
+
+        self.goal_pos = np.array(
             [
                 np.random.randint(0, self.grid_size - 1),
                 np.random.randint(0, self.grid_size - 1),
             ]
         )
-        if goal is None:
-            self.goal_pos = np.array(
-                [
-                    np.random.randint(0, self.grid_size - 1),
-                    np.random.randint(0, self.grid_size - 1),
-                ]
-            )
-        else:
-            self.goal_pos = np.array(goal)
         self.step_count = 0
         return self._get_obs(), {}
 
     def _get_obs(self):
-        return (
-            np.array([*self.agent_pos, *self.goal_pos], dtype=np.float32)
-        )
+        return np.array([*self.agent_pos, *self.goal_pos], dtype=np.int32)
 
     def step(self, action):
         self.step_count += 1
+        # old_agent_pos = self.agent_pos.copy()  # Store old position
 
         if action == 0:  # thrust up
             self.agent_pos[1] += self.step_size
@@ -68,11 +60,11 @@ class HelicopterControlEnv(gym.Env):
         truncated = self.step_count >= self.max_step_size
 
         if done:
-            reward = 100
+            reward = 1000  # Reduced sparse reward
+            print("The agent has hit the goal!")
         else:
-            # distance = np.linalg.norm(self.agent_pos - self.goal_pos)  # Fixed typo
             distance = np.sum(np.abs(self.agent_pos - self.goal_pos))
-            reward = -1 - 0.1 * distance
+            reward = -1 - 0.5 * distance  # Increased dense penalty
 
         return self._get_obs(), reward, done, truncated, {}
 
